32429874dd859da3cbce7ab76e448f4239a71345
Yuzuki <yuzukimt462525@gmail.com>
2026-07-18 23:23:54 +0900

    feat(logging): message系ハンドラファクトリを実装する
    - createMessageLogHandlers: onMessageCreate/Update/Delete を提供
    - bot発言スキップ、update無変化スキップ、writeLogEvent失敗時のcatch+console.error握り潰しを実装
    - message-events.js / message-log-handlers.js を index.ts からexport

--- packages/logging/src/index.ts
+++ packages/logging/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./application/log-stream.js";
 export * from "./application/log-writer.js";
+export * from "./discord/message-events.js";
+export * from "./discord/message-log-handlers.js";
 export * from "./domain/realtime-policy.js";
